1e290b1677e746b59afcf1e34526d3f7ba6b9568
S1queira975 <otavio09siq@gmail.com>
2026-03-21 15:58:47 -0300

    feat/extrair-texto-da-mensagem

---
+++ src/whatsapp/menssagem-type.js
@@ -0,0 +1,17 @@
+function GetMensagemText(message){ 
+    if(message.message?.conversation){
+        return message.message.conversation;
+    }
+    if(message.message?.extendedTextMessage?.text){
+        return message.message.extendedTextMessage.text;
+    }
+    if(message.message?.imageMessage?.caption){
+        return message.message.imageMessage.caption;
+    }
+    if(message.message?.videoMessage?.caption){
+        return message.message.videoMessage.caption;
+    }
+}
+    module.exports = {
+        GetMensagemText
+};
