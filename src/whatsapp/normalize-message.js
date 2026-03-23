71bf1770b7849f86fd1dd360888f755ad746b2b6
Camilli <camilli.santos2015@gmail.com>
2026-03-22 21:19:02 -0300

    feat(mensagem): garantir dados válidos no normalizeMessage

--- src/whatsapp/normalize-message.js
+++ src/whatsapp/normalize-message.js
@@ -2,12 +2,15 @@ const { GetMessageText } = require('./message-text');
 const { getMessageType } = require('./message-type');
 
 function normalizeMessage(message){
-    const text = GetMessageText(message)
-    const type = getMessageType(message)
-    const from = message.key.remoteJid
-    return {
-        text, type, from 
-    }
+    const normalize = { text : GetMessageText(message)
+     , type : getMessageType(message)
+    , from : message.key?.remoteJid }
+
+    if (!normalize.type || !normalize.from ){
+        return null; 
+    } 
+
+    return normalize
 }
 module.exports = {
     normalizeMessage
